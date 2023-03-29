079a4d59499f28d96c8d00a20b1a017d77fbb55c
lolz <102578246+TheShogunsDescent@users.noreply.github.com>
2023-03-29 17:44:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas=createCanvas(500,500)
+    canvas.position(110,250)
+}
+function draw() {
+    rect(0, 0, 55, 500, 20);
+    rect(445, 0, 55, 500, 20);
+    rect(-100, 100, 55, 500, 20);
+   
+}
